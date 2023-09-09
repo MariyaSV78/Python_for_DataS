fdc9248cfae2b076b64b228baebb9ce93a8797e6
Mariya SOSNOVA <msosnova@made-f0Br1s6.clusters.42paris.fr>
2023-09-09 17:27:14 +0200

    ex2

--- Jour0_starting/ex01/format_ft_time.py
+++ Jour0_starting/ex01/format_ft_time.py
@@ -1,2 +1,7 @@
-import time as tm
+import time
 
+data = time.time()
+today = time.gmtime()
+
+print("Seconde since January 1 1970: " + f'{data:,.4f}' + " or " + f'{data:,.2e}' + " in scientific notation")
+print(time.strftime("%b %d %Y", today))
