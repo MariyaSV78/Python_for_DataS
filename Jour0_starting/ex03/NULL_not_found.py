40a563e624dfff3649e2221b95d5936f7fa7129f
Mariya SOSNOVA <msosnova@made-f0Br1s6.clusters.42paris.fr>
2023-09-09 17:59:51 +0200

    ex3

---
+++ Jour0_starting/ex03/NULL_not_found.py
@@ -0,0 +1,16 @@
+def NULL_not_found(object: any) -> int:
+	"""
+		Function that prints the object type of all types of "Null".
+		Return  0 if it goes well;
+				1 in case of error.
+		Your function needs to print all types of NULL. 
+	"""
+	if (object is None)
+		print ("Nothing: None", type(object))
+	elif ()
+Cheese: nan <class 'float'>$
+Zero: 0 <class 'int'>$
+Empty: <class 'str'>$
+Fake: False <class 'bool'>$
+Type not Found$
+1$
